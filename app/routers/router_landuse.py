f1749134ec742c84aeb48736e423a55173bb79e3
Mvin8 <skitless1229@gmail.com>
2024-11-27 15:40:24 +0300

    feat: add landuse

--- app/routers/router_landuse.py
+++ app/routers/router_landuse.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends, HTTPException, Query, Header, BackgroundTasks, Request
 import json
 import geopandas as gpd
 from pydantic_geojson import PolygonModel
@@ -7,19 +7,53 @@ from popframe.method.landuse_assessment import LandUseAssessment
 from popframe.models.region import Region
 from app.utils.data_loader import get_region_model
 from app.utils.config import DEFAULT_CRS
+from app.utils.config import DEFAULT_CRS, URBAN_API
+from app.utils.auth import verify_token 
+import requests
+
 landuse_router = APIRouter(prefix="/landuse", tags=["Landuse data"])
 
 # Land Use Data Endpoints
 @landuse_router.post("/get_landuse_data", response_model=Dict[str, Any])
-async def get_landuse_data_endpoint(polygon : PolygonModel, region_model: Region = Depends(get_region_model)):
+async def get_landuse_data_endpoint(
+    region_model: Region = Depends(get_region_model),
+    project_scenario_id: int | None = Query(None, description="ID сценария cценария"),
+    token: str = Depends(verify_token)
+    ):
     try:
-        urbanisation = LandUseAssessment(region=region_model)
-        polygon_feature = {
-        'type': 'Feature',
-        'geometry' : polygon.model_dump(),
-        'properties': {}
+        # Getting project_id and additional information based on scenario_id
+        scenario_response = requests.get(
+            f"{URBAN_API}/scenarios/{project_scenario_id}",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        if scenario_response.status_code != 200:
+            raise Exception("Error retrieving scenario information")
+        
+        scenario_data = scenario_response.json()
+        project_id = scenario_data.get("project", {}).get("project_id")
+        if project_id is None:
+            raise Exception("Project ID is missing in scenario data.")
+        
+        # Retrieving territory geometry
+        territory_response = requests.get(
+            f"{URBAN_API}/projects/{project_id}/territory",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        if territory_response.status_code != 200:
+            raise Exception("Error retrieving territory geometry")
+        
+        # Extracting only the polygon geometry
+        territory_data = territory_response.json()
+        territory_geometry = territory_data["geometry"]
+
+        # Converting the territory geometry to GeoDataFrame
+        territory_feature = {
+            'type': 'Feature',
+            'geometry': territory_geometry,
+            'properties': {}
         }
-        polygon_gdf = gpd.GeoDataFrame.from_features([polygon_feature], crs=DEFAULT_CRS)
+        urbanisation = LandUseAssessment(region=region_model)
+        polygon_gdf = gpd.GeoDataFrame.from_features([territory_feature], crs=DEFAULT_CRS)
         landuse_data = urbanisation.get_landuse_data(territories=polygon_gdf)
         return json.loads(landuse_data.to_json())
     except Exception as e:
